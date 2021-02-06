8d5efad60de1ca90bf4f7e03e09e838ba0b9edb3
Гусейн <manblax@yandex.ru>
2021-02-06 19:23:20 +0300

    Добавить дополнительные элементы

--- src/components/Wysiwig.vue
+++ src/components/Wysiwig.vue
@@ -11,12 +11,12 @@
           <use xlink:href="@/assets/sprite.svg#go-back-arrow"/>
         </svg>
       </Button>
-      <Button @click="heading">
+      <Button @click="tag" data-tag="h1">
         <svg>
           <use xlink:href="@/assets/sprite.svg#t-icon"/>
         </svg>
       </Button>
-      <Button @click="paragraph">
+      <Button @click="tag" data-tag="p">
         <svg>
           <use xlink:href="@/assets/sprite.svg#paragraph"/>
         </svg>
@@ -26,12 +26,42 @@
           <use xlink:href="@/assets/sprite.svg#image"/>
         </svg>
       </Button>
-      <Button @click="copyHtml">
-        Скопировать HTML
+      <Button @click="tag" data-tag="h2">
+        <svg>
+          <use xlink:href="@/assets/sprite.svg#heading-2"/>
+        </svg>
       </Button>
+      <Button @click="tag" data-tag="h3">
+        <svg>
+          <use xlink:href="@/assets/sprite.svg#heading-3"/>
+        </svg>
+      </Button>
+      <Button @click="tag" data-tag="h4">
+        <svg>
+          <use xlink:href="@/assets/sprite.svg#heading-4"/>
+        </svg>
+      </Button>
+      <Button @click="bold">
+        <svg>
+          <use xlink:href="@/assets/sprite.svg#bold"/>
+        </svg>
+      </Button>
+      <Button @click="align" data-align="justifyCenter">
+        <svg>
+          <use xlink:href="@/assets/sprite.svg#align-center"/>
+        </svg>
+      </Button>
+      <Button @click="align" data-align="justifyLeft">
+        <svg>
+          <use xlink:href="@/assets/sprite.svg#align-left"/>
+        </svg>
+      </Button>
+      <Button @click="copyHtml">Скопировать HTML</Button>
     </div>
     <FileInput @change="readFile"></FileInput>
-    <div contenteditable class="editor" ref="editor">efewfe</div>
+    <div contenteditable class="editor" ref="editor">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, recusandae?
+    </div>
   </div>
 </template>
 
@@ -46,22 +76,22 @@ export default {
     FileInput
   },
   methods: {
-    heading() {
-      console.log('heading')
-      document.execCommand('formatBlock', false, 'h1');
+    tag(event) {
+      console.log('tag')
+      document.execCommand('formatBlock', false, event.currentTarget.dataset.tag);
     },
-    paragraph() {
-      console.log('p')
-      document.execCommand('formatBlock', false, 'p');
+    bold() {
+      document.execCommand('bold');
+    },
+    align(event) {
+      console.log('align')
+      document.execCommand(event.currentTarget.dataset.align);
     },
     copyHtml() {
       const html = this.$refs.editor.innerHTML;
       navigator.clipboard.writeText(html);
       alert(html);
       console.log(html);
-    },
-    insertImage() {
-
     },
     undo() {
       document.execCommand('undo', false, '')
@@ -79,9 +109,6 @@ export default {
         document.execCommand('insertImage', false, src);
       }
     },
-    clear() {
-      document.execCommand('clear');
-    },
     readFile(file) {
       const fileUrl = URL.createObjectURL(file);
       this.$refs.editor.focus();
@@ -96,7 +123,7 @@ export default {
 
 .controls {
   display: grid;
-  grid-template-columns: repeat(6, auto);
+  grid-template-columns: repeat(12, auto);
   justify-content: start;
   align-content: center;
   grid-gap: 24px;
